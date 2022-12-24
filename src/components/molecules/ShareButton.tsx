e47cc4941eebbe577140574bb720bfa40f4481fd
Ferb <88365695+devops0019@users.noreply.github.com>
2022-12-24 15:04:19 +0000

    share button changes

--- src/components/molecules/ShareButton.tsx
+++ src/components/molecules/ShareButton.tsx
@@ -1,18 +1,11 @@
 import { useState } from "react";
-import Modal from 'react-modal';
 import { LinkedinShareButton, FacebookShareButton, WhatsappShareButton, TwitterShareButton, EmailShareButton } from 'react-share';
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import {
-   faYoutube,
-   faInstagram,
-   // faEnvelope,
    faFacebookF,
    faLinkedin,
    faWhatsapp,
    faTwitter,
-   faX
 } from "@fortawesome/free-brands-svg-icons";
 import {
    faEnvelope
@@ -22,111 +15,67 @@ interface IShareButton {
    title: string,
    url: string,
 }
-const customStyles = {
-   content: {
-     top: '50%',
-     left: '50%',
-     right: 'auto',
-     bottom: 'auto',
-     marginRight: '-50%',
-     transform: 'translate(-50%, -50%)',
-     maxWidth: '500px'
-   },
- };
 
 export default function ShareButton(props: IShareButton) {
-   const [modalIsOpen, setIsOpen] = useState(false);
-   // const onCloseClick = () => {
-   //    props.onCloseClick()
-   // }
-   function openModal() {
-      setIsOpen(true);
-   }
-   function afterOpenModal() {
-      // references are now sync'd and can be accessed.
-      // subtitle.style.color = '#f00';
-   }
-   function closeModal() {
-      setIsOpen(false);
-   }
-
+   const [activeTab, setActiveTab] = useState(false)
    return (
       <>
-         <div className="flex justify-between md:mb-4 cursor-pointer" onClick={openModal}>
-            {/* <span className="text-base font-bold">{date}</span> */}
-            <span className="text-green-600 hover:text-green-900 font-normal text-base flex items-center gap-2 share-button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-reply-fill" viewBox="0 0 16 16">
-               <path d="M5.921 11.9 1.353 8.62a.719.719 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z" />
-            </svg>SHARE</span>
-         </div>
-
-         <Modal
-            isOpen={modalIsOpen}
-            onAfterOpen={afterOpenModal}
-            onRequestClose={closeModal}
-            style={customStyles}
-            contentLabel="Example Modal"
-         >
-            <div>
-            <button type='button' onClick={closeModal} className="absolute top-5 right-5 opacity-60 hover:opacity-100">
-               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-               </svg>
-            </button>
-               <p className="text-lg mb-3 font-bold">{'Share '}</p>
-               <p className="mb-4 text-sm">{props.title}</p>
-               <div className="px-3 py-3 max-w-full break-word">
-                  <code>{props.url}</code>
+         <div className="flex items-center share-btn">
+            <div className="flex justify-between cursor-pointer " onClick={() => setActiveTab(prevState => !prevState)}>
+               {/* <span className="text-base font-bold">{date}</span> */}
+               <span className="text-green-600 hover:text-green-900 font-normal text-base flex items-center gap-2 share-button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-reply-fill" viewBox="0 0 16 16">
+                  <path d="M5.921 11.9 1.353 8.62a.719.719 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z" />
+               </svg>SHARE</span>
+            </div>
+            <div className={`${activeTab ? 'is-opened' : 'is-closed'} ml-6 grid grid-cols-5 gap-3 justify-center text-center text-sm text-gray-500 border border-gray-400 rounded-md px-2 py-1`}>
+               <div className="grid__col">
+                  <FacebookShareButton {...props}>
+                     <FontAwesomeIcon
+                        id="icons"
+                        className="text-dark"
+                        icon={faFacebookF}
+                     />
+                  </FacebookShareButton>
                </div>
-               <div className="grid grid-cols-6 gap-4 mt-4 justify-center text-center text-xl">
-                  <div className="grid__col">
-                     <FacebookShareButton {...props}>
-                        <FontAwesomeIcon
-                           id="icons"
-                           className="text-dark"
-                           icon={faFacebookF}
-                        />
-                     </FacebookShareButton>
-                  </div>
-                  <div className="grid__col">
-                     <LinkedinShareButton {...props}>
-                        <FontAwesomeIcon
-                           id="icons"
-                           className="text-dark"
-                           icon={faLinkedin}
-                        />
-                     </LinkedinShareButton>
-                  </div>
-                  <div className="grid__col">
-                     <WhatsappShareButton {...props}>
-                        <FontAwesomeIcon
-                           id="icons"
-                           className="text-dark"
-                           icon={faWhatsapp}
-                        />
-                     </WhatsappShareButton>
-                  </div>
-                  <div className="grid__col">
-                     <EmailShareButton {...props}>
-                        {/* <FontAwesomeIcon icon="fa-solid fa-envelope" /> */}
-                        <FontAwesomeIcon
-                           id="icons"
-                           className="text-dark"
-                           icon={faEnvelope}
-                        />
-                     </EmailShareButton>
-                  </div>
-                  <div className="grid__col">
-                     <TwitterShareButton {...props}>
-                        <FontAwesomeIcon
-                           id="icons"
-                           className="text-dark"
-                           icon={faTwitter}
-                        />
-                     </TwitterShareButton>
-                  </div>
+               <div className="grid__col">
+                  <LinkedinShareButton {...props}>
+                     <FontAwesomeIcon
+                        id="icons"
+                        className="text-dark"
+                        icon={faLinkedin}
+                     />
+                  </LinkedinShareButton>
+               </div>
+               <div className="grid__col">
+                  <WhatsappShareButton {...props}>
+                     <FontAwesomeIcon
+                        id="icons"
+                        className="text-dark"
+                        icon={faWhatsapp}
+                     />
+                  </WhatsappShareButton>
+               </div>
+               <div className="grid__col">
+                  <EmailShareButton {...props}>
+                     {/* <FontAwesomeIcon icon="fa-solid fa-envelope" /> */}
+                     <FontAwesomeIcon
+                        id="icons"
+                        className="text-dark"
+                        icon={faEnvelope}
+                     />
+                  </EmailShareButton>
+               </div>
+               <div className="grid__col">
+                  <TwitterShareButton {...props}>
+                     <FontAwesomeIcon
+                        id="icons"
+                        className="text-dark"
+                        icon={faTwitter}
+                     />
+                  </TwitterShareButton>
                </div>
             </div>
-         </Modal>
+         </div>
 
       </>
    )
