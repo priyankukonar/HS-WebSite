aff2e8cf1461c647e3e0d76f6d88d4fe1d30afe9
Ferb <88365695+devops0019@users.noreply.github.com>
2022-12-24 15:18:56 +0000

    toggle share buttons

--- src/components/molecules/ShareButton.tsx
+++ src/components/molecules/ShareButton.tsx
@@ -27,7 +27,7 @@ export default function ShareButton(props: IShareButton) {
                   <path d="M5.921 11.9 1.353 8.62a.719.719 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z" />
                </svg>SHARE</span>
             </div>
-            <div className={`${activeTab ? 'is-opened' : 'is-closed'} ml-6 grid grid-cols-5 gap-3 justify-center text-center text-sm text-gray-500 border border-gray-400 rounded-md px-2 py-1`}>
+            <div className={`${activeTab ? 'is-opened' : ''} share-btn__menu ml-6 grid grid-cols-5 gap-3 justify-center text-center text-sm text-gray-500 border border-gray-400 rounded-md px-2 py-1`}>
                <div className="grid__col">
                   <FacebookShareButton {...props}>
                      <FontAwesomeIcon
