a4f9dd36425c5da5e43fd64cb98c53e9b15143ec
priyanku <priyanku@haystackanalytics.in>
2024-01-20 11:09:01 +0530

    add ceo message

---
+++ src/pages/message.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import AppTemplate from "../components/templates/AppTemplate";
+import Section from '../components/atoms/Section'
+import {Title} from "../components/atoms/Title";
+
+const Message = () => {
+   return (
+     
+         <Section>
+            <Title>Contact Us</Title>
+           
+            <table width="650" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" align="center">
+                  <tr>
+                     <td>
+                              hi there 
+                     </td>
+                  </tr>
+               </table>
+
+         </Section>
+ 
+   );
+};
+
+export default Message;
