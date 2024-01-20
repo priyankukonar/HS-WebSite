25c4bd798d13f3fb7022720d5b72fc6cd8345637
omegauser <arpan.bagchi@haystackanalytics.in>
2024-01-20 12:14:32 +0530

    ceo message updated

--- src/pages/message.tsx
+++ src/pages/message.tsx
@@ -3,19 +3,36 @@ import AppTemplate from "../components/templates/AppTemplate";
 import Section from '../components/atoms/Section'
 import {Title} from "../components/atoms/Title";
 
+
 const Message = () => {
    return (
      
          <Section>
-            <Title>Contact Us</Title>
-           
-            <table width="650" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" align="center">
-                  <tr>
-                     <td>
-                              hi there 
-                     </td>
-                  </tr>
-               </table>
+
+            <div className="w-[650px] pt-5"
+            style={{"margin":"auto"}}
+>
+               <div className="grid grid-rows-1 gap-1">
+                  <div className="col-span-2 h-[250px]">
+                     <img src="/assets/messageboard/img1.jpg" className="w-full h-[250px]" />
+
+                  </div>
+                  <div className="grid grid-cols-3 col-span-2 border">
+                     <div className=" col-span-1">
+                        <img src="/assets/messageboard/img2a.jpg" className="w-full" />
+
+                     </div>
+                     <div className=" col-span-2 ">
+                        <img src="/assets/messageboard/img2b.jpg" className="w-full" />
+
+                     </div>
+                  </div>
+               
+               </div>
+            
+            </div>
+
+
 
          </Section>
  
